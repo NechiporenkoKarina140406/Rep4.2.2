48aa3935ea7c60a121887b1a4d8a5b06e74e52e9
Karina <karinanechiporenko1206@gmail.com>
2024-02-15 20:19:57 +0200

    Functions and their call 3

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from utils import check_prime
+from utils import is_power_of_five
+from utils import is_power_of_two
 
--- utils.py
+++ utils.py
@@ -12,3 +12,33 @@ if n < 2:
     quit()
 elif n == 2:
     print("Це  просте число")
+# Визначаємо , чи є число степенем 5
+def is_power_of_five(num):
+    power = 0
+    while 5 ** power <= num:
+        if 5 ** power == num:
+            return True
+        power += 1
+    return False
+
+# Приклад використання
+number = int(input("Введіть число: "))
+if is_power_of_five(number):
+    print(f"{number} є степенем 5")
+else:
+    print(f"{number} не є степенем 5")
+# чи є число степенем 2
+def is_power_of_two(num):
+    while num > 1:
+        if num % 2 != 0:
+            return False
+        num //= 2
+    return True
+
+# Приклад використання
+number = int(input("Введіть число: "))
+if is_power_of_two(number):
+    print(f"{number} є степенем 2")
+else:
+    print(f"{number} не є степенем 2")
+
